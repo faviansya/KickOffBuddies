9d8697b7825f97ba419cee0b64e16228d368f939
faviansya <faviansya@alphatech.id>
2019-03-28 19:23:22 +0700

    Tambah Feature Pemain

--- blueprints/pemain/__init__.py
+++ blueprints/pemain/__init__.py
@@ -9,19 +9,26 @@ class Pemain(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
+    name = db.Column(db.String(100))
+    user_type = db.Column(db.String(100))
+    created_time = db.Column(db.String(100))
 
     response_field = {
         'id': fields.Integer,
-        'password': fields.String,
         'username': fields.String,
+        'password': fields.String,
+        'name': fields.String,
+        'user_type': fields.String,
+        'created_time': fields.String,
 
     }
 
-    def __init__(self, username, password, name, urlimage, alamat, status, level,transaction,kota):
+    def __init__(self, username, password, name, user_type, created_time):
         self.username = username
         self.password = password
-
+        self.name = name
+        self.user_type = user_type
+        self.created_time = created_time
 
     def __repr__(self):
         return '<Pemain %r>' % self.id
-
