c8f5ccf85a035a9994bb4fe6f644fb13e4dc93fb
arafat <arafat@alphatech.id>
2019-03-29 21:56:02 +0700

    pass validation

--- blueprints/pebisnis/resources.py
+++ blueprints/pebisnis/resources.py
@@ -5,6 +5,7 @@ from . import *
 from blueprints import db
 from flask_jwt_extended import jwt_required, get_jwt_claims
 import datetime
+from password_strength import PasswordPolicy
 
 bp_pebisnis = Blueprint('pebisnis', __name__)
 api = Api(bp_pebisnis)
@@ -20,7 +21,13 @@ class PebisnisResource(Resource):
         qry = Pebisnis.query.get(id) #select * from where id = id
         return {"status": "200 OK", "message": "Your user profile is on display", "data": marshal(qry, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}   
 
-    def post(self): #for pebisnis to register       
+    def post(self): #for pebisnis to register 
+        policy = PasswordPolicy.from_names(
+            length=6,
+            uppercase=1,
+            numbers=1,
+            special=1,
+        )
         parser = reqparse.RequestParser()
         parser.add_argument('username', location='json')
         parser.add_argument('password', location='json')
@@ -31,16 +38,27 @@ class PebisnisResource(Resource):
         parser.add_argument('phone_no', location='json')
         parser.add_argument('address', location='json')
         parser.add_argument('deskripsi', location='json')
-        args = parser.parse_args() 
-        user_type = 'pebisnis'
-        password = hashlib.md5(args['password'].encode()).hexdigest()
-        user_new = Pebisnis('pebisnis', args['username'], password, args['name'], args['nama_tempat'], args['lapangan'], args['email'], args['phone_no'], args['address'], args['deskripsi'], str(datetime.datetime.now()))
-        db.session.add(user_new) #insert the input data into the database
-        db.session.commit() 
-        return {"code": 200, "message": "OK, your user profile has been created", "data":marshal(user_new, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}   
-    
+        args = parser.parse_args()
+
+        validation = policy.test(args['password'])
+        
+        if validation == [] :
+            user_type = 'pebisnis'
+            password = hashlib.md5(args['password'].encode()).hexdigest()
+            user_new = Pebisnis('pebisnis', args['username'], password, args['name'], args['nama_tempat'], args['lapangan'], args['email'], args['phone_no'], args['address'], args['deskripsi'], str(datetime.datetime.now()))
+            db.session.add(user_new) #insert the input data into the database
+            db.session.commit() 
+            return {"code": 200, "message": "OK, your user profile has been created", "data":marshal(user_new, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}   
+        return {'status' : 'Password Invalid'}, 400, {'Content_type' : 'application/json'}
+
     @jwt_required 
     def put(self, id=None):
+        policy = PasswordPolicy.from_names(
+            length=6,
+            uppercase=1,
+            numbers=1,
+            special=1,
+        )
         parser = reqparse.RequestParser()
         parser.add_argument('password', location='json')
         parser.add_argument('name', location='json')
@@ -51,29 +69,34 @@ class PebisnisResource(Resource):
         parser.add_argument('address', location='json')
         parser.add_argument('deskripsi', location='json')
         args = parser.parse_args()
-        password = hashlib.md5(args['password'].encode()).hexdigest()
-        id = get_jwt_claims()['id']
-        qry_user = Pebisnis.query.get(id)
-        if qry_user is not None and get_jwt_claims()['id'] == id:
-            if args['lapangan'] is not None:
-                qry_user.lapangan = args['lapangan']
-            if args['nama_tempat'] is not None:
-                qry_user.nama_tempat = args['nama_tempat']
-            if args['password'] is not None:
-                qry_user.password = password
-            if args['name'] is not None:
-                qry_user.name = args['name']
-            if args['email'] is not None:
-                qry_user.email = args['email']
-            if args['phone_no'] is not None:
-                qry_user.phone_no = args['phone_no']
-            if args['address'] is not None:
-                qry_user.address = args['address']
-            if args['deskripsi'] is not None:
-                qry_user.deskripsi = args['deskripsi']
-            db.session.commit()
-            return {"code": 200, "message": "OK, your user profile has been created", "data": marshal(qry_user, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}
-        return {"code": 404, "message": "Failed to edit. Wrong username or password"}, 404, {'Content-Type': 'application/json'}
+
+        validation = policy.test(args['password'])
+
+        if validation == [] :
+            password = hashlib.md5(args['password'].encode()).hexdigest()
+            id = get_jwt_claims()['id']
+            qry_user = Pebisnis.query.get(id)
+            if qry_user is not None and get_jwt_claims()['id'] == id:
+                if args['lapangan'] is not None:
+                    qry_user.lapangan = args['lapangan']
+                if args['nama_tempat'] is not None:
+                    qry_user.nama_tempat = args['nama_tempat']
+                if args['password'] is not None:
+                    qry_user.password = password
+                if args['name'] is not None:
+                    qry_user.name = args['name']
+                if args['email'] is not None:
+                    qry_user.email = args['email']
+                if args['phone_no'] is not None:
+                    qry_user.phone_no = args['phone_no']
+                if args['address'] is not None:
+                    qry_user.address = args['address']
+                if args['deskripsi'] is not None:
+                    qry_user.deskripsi = args['deskripsi']
+                db.session.commit()
+                return {"code": 200, "message": "OK, your user profile has been created", "data": marshal(qry_user, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}
+            return {"code": 404, "message": "Failed to edit. Wrong username or password"}, 404, {'Content-Type': 'application/json'}
+        return {'status' : 'Password Invalid'}, 400, {'Content_type' : 'application/json'}
 
     @jwt_required 
     def delete(self, id):
