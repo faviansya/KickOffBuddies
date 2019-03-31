c0968193fb9956703737627b0dd6b087861de7fd
fatmajanna <fatma.janna.utp@gmail.com>
2019-03-31 13:50:13 +0700

    update edit pemain or pebisnis

--- blueprints/pebisnis/resources.py
+++ blueprints/pebisnis/resources.py
@@ -38,6 +38,7 @@ class PebisnisResource(Resource):
         parser.add_argument('phone_no', location='json')
         parser.add_argument('address', location='json')
         parser.add_argument('deskripsi', location='json')
+        parser.add_argument('url_image', location='json')
         args = parser.parse_args()
 
         validation = policy.test(args['password'])
@@ -45,14 +46,14 @@ class PebisnisResource(Resource):
         if validation == [] :
             user_type = 'pebisnis'
             password = hashlib.md5(args['password'].encode()).hexdigest()
-            user_new = Pebisnis('pebisnis', args['username'], password, args['name'], args['nama_tempat'], args['lapangan'], args['email'], args['phone_no'], args['address'], args['deskripsi'], str(datetime.datetime.now()))
+            user_new = Pebisnis('pebisnis', args['username'], password, args['name'], args['nama_tempat'], args['lapangan'], args['email'], args['phone_no'], args['address'], args['deskripsi'], str(datetime.datetime.now()), args['url_image'])
             db.session.add(user_new) #insert the input data into the database
             db.session.commit() 
             return {"code": 200, "message": "OK, your user profile has been created", "data":marshal(user_new, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}   
         return {'status' : 'Password Invalid'}, 400, {'Content_type' : 'application/json'}
 
     @jwt_required 
-    def put(self, id=None):
+    def put(self):
         policy = PasswordPolicy.from_names(
             length=6,
             uppercase=1,
@@ -68,34 +69,37 @@ class PebisnisResource(Resource):
         parser.add_argument('phone_no', location='json')
         parser.add_argument('address', location='json')
         parser.add_argument('deskripsi', location='json')
+        parser.add_argument('url_image', location='json')
         args = parser.parse_args()
+        
+        qry_user = Pebisnis.query.get(get_jwt_claims()['id'])
 
-        validation = policy.test(args['password'])
-
-        if validation == [] :
-            password = hashlib.md5(args['password'].encode()).hexdigest()
-            id = get_jwt_claims()['id']
-            qry_user = Pebisnis.query.get(id)
-            if qry_user is not None and get_jwt_claims()['id'] == id:
-                if args['lapangan'] is not None:
-                    qry_user.lapangan = args['lapangan']
-                if args['nama_tempat'] is not None:
-                    qry_user.nama_tempat = args['nama_tempat']
-                if args['password'] is not None:
-                    qry_user.password = password
-                if args['name'] is not None:
-                    qry_user.name = args['name']
-                if args['email'] is not None:
-                    qry_user.email = args['email']
-                if args['phone_no'] is not None:
-                    qry_user.phone_no = args['phone_no']
-                if args['address'] is not None:
-                    qry_user.address = args['address']
-                if args['deskripsi'] is not None:
-                    qry_user.deskripsi = args['deskripsi']
-                db.session.commit()
-                return {"code": 200, "message": "OK, your user profile has been created", "data": marshal(qry_user, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}
-            return {"code": 404, "message": "Failed to edit. Wrong username or password"}, 404, {'Content-Type': 'application/json'}
+        if args['password'] is not None:
+            validation = policy.test(args['password'])
+            if validation == [] :
+                password = hashlib.md5(args['password'].encode()).hexdigest()
+                qry.password = password
+        else:
+            if args['lapangan'] is not None:
+                qry_user.lapangan = args['lapangan']
+            if args['nama_tempat'] is not None:
+                qry_user.nama_tempat = args['nama_tempat']
+            if args['password'] is not None:
+                qry_user.password = password
+            if args['name'] is not None:
+                qry_user.name = args['name']
+            if args['email'] is not None:
+                qry_user.email = args['email']
+            if args['phone_no'] is not None:
+                qry_user.phone_no = args['phone_no']
+            if args['address'] is not None:
+                qry_user.address = args['address']
+            if args['deskripsi'] is not None:
+                qry_user.deskripsi = args['deskripsi']
+            if args['url_image'] is not None:
+                qry_user.url_image = args['url_image']
+            db.session.commit()
+            return {"code": 200, "message": "OK, your user profile has been created", "data": marshal(qry_user, Pebisnis.response_field)}, 200, {'Content-Type': 'application/json'}
         return {'status' : 'Password Invalid'}, 400, {'Content_type' : 'application/json'}
 
     @jwt_required 
